f041326a18f1fc9633e21f131de46fb0bc7fd2f2
aletheatoh <alethea.toh@duke.edu>
2019-11-13 11:44:21 -0500

    added pareto frontier

---
+++ pareto_frontier.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def simple_cull(inputPoints):
+    paretoPoints = set()
+    candidateRowNr = 0
+    dominatedPoints = set()
+    while True:
+        candidateRow = inputPoints[candidateRowNr]
+        inputPoints.remove(candidateRow)
+        rowNr = 0
+        nonDominated = True
+        while len(inputPoints) != 0 and rowNr < len(inputPoints):
+            row = inputPoints[rowNr]
+            if dominates(candidateRow, row):
+                # If it is worse on all features remove the row from the array
+                inputPoints.remove(row)
+                dominatedPoints.add(tuple(row))
+            elif dominates(row, candidateRow):
+                nonDominated = False
+                dominatedPoints.add(tuple(candidateRow))
+                rowNr += 1
+            else:
+                rowNr += 1
+
+        if nonDominated:
+            # add the non-dominated point to the Pareto frontier
+            paretoPoints.add(tuple(candidateRow))
+
+        if len(inputPoints) == 0:
+            break
+    return paretoPoints, dominatedPoints
+
+def dominates(row, candidateRow):
+    sum = 0
+    if row[0] <= candidateRow[0]: sum += 1 # duration
+    if row[1] <= candidateRow[1]: sum += 1 # price
+    if row[2] >= candidateRow[2]: sum += 1 # frequency
+
+    return sum
